0d755483b766ab072f0781cebcc44a7fb402db36
ezamlinux <sanctorum.thomas@gmail.com>
2017-12-24 13:49:36 +0100

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 var app = express();
 
 app.use((req, res, next) => next());
-
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.listen(5000, () => console.log('server Running at: http://localhost:5000'));
+app.listen(app.get('port'), () => console.log('server Running'));
